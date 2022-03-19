8444fce829df9748544090cd816b6727ec786232
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-03-19 14:16:14 +0500

    fake Db to see how app works

---
+++ src/db/fakeDb.js
@@ -0,0 +1,234 @@
+export const db = [
+    {
+        "name": "Никита",
+        "surname": "Негляд",
+        "phone": "+998974406360",
+        "payment": 300,
+        "id": "Wq2jG",
+        "pack": {
+            "value": "starter",
+            "title": "STARTER пакет"
+        }
+    },
+    {
+        "name": "Исмаил",
+        "surname": "Гаспринский",
+        "phone": "+998998152821",
+        "payment": 1000,
+        "id": "bxoFW",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Павловский",
+        "surname": "Георгий",
+        "phone": "+998956331533",
+        "payment": 500,
+        "id": "oqIUN",
+        "payed": 500,
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Мария",
+        "surname": "Кусякина",
+        "phone": "+998993688998",
+        "payment": 300,
+        "id": "IT1Kb",
+        "pack": {
+            "value": "starter",
+            "title": "STARTER пакет"
+        }
+    },
+    {
+        "name": "Севастьянов",
+        "surname": "Даниил",
+        "phone": "+998954587458",
+        "payment": 1000,
+        "id": "kD7Or",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Тарасов",
+        "surname": "Артём",
+        "phone": "+998903256789",
+        "payment": 800,
+        "id": "KMv8O",
+        "pack": {
+            "value": "premium",
+            "title": "PREMUIM пакет"
+        }
+    },
+    {
+        "name": "Семенова",
+        "surname": "Кира",
+        "phone": "+998908556677",
+        "payment": 1000,
+        "id": "U7ibS",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Соболева",
+        "surname": "Софья",
+        "phone": "+998971274568",
+        "payment": 800,
+        "id": "odLqf",
+        "pack": {
+            "value": "premium",
+            "title": "PREMUIM пакет"
+        }
+    },
+    {
+        "name": "Максимов",
+        "surname": "Артём",
+        "phone": "+998908412356",
+        "payment": 1000,
+        "id": "NWeFl",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Иванова",
+        "surname": "Элина",
+        "phone": "+998953218965",
+        "payment": 300,
+        "id": "3qROd",
+        "pack": {
+            "value": "starter",
+            "title": "STARTER пакет"
+        }
+    },
+    {
+        "name": "Жарова",
+        "surname": "Милана",
+        "phone": "+998712364789",
+        "payment": 300,
+        "id": "ZZxZC",
+        "pack": {
+            "value": "starter",
+            "title": "STARTER пакет"
+        }
+    },
+    {
+        "name": "Панин",
+        "surname": "Илья",
+        "phone": "+998954561234",
+        "payment": 1000,
+        "id": "wqOFo",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Черная",
+        "surname": "Анна",
+        "phone": "+998956766776",
+        "payment": 1000,
+        "id": "oMYrj",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Воробьева",
+        "surname": "Злата",
+        "phone": "+998909359539",
+        "payment": 1000,
+        "id": "yoTsX",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Алишер",
+        "surname": "Батыров",
+        "phone": "+998994409899",
+        "payment": 1000,
+        "id": "XTXyj",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Воробьева",
+        "surname": "Злата",
+        "phone": "+998981256509",
+        "payment": 1000,
+        "id": "y4Hxp",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Анисимов",
+        "surname": "Иван",
+        "phone": "+998941564545",
+        "payment": 500,
+        "id": "lin8a",
+        "pack": {
+            "value": "standart",
+            "title": "STANDART пакет"
+        }
+    },
+    {
+        "name": "Панин",
+        "surname": "Илья",
+        "phone": "+998712237623",
+        "payment": 500,
+        "id": "eaOVE",
+        "pack": {
+            "value": "standart",
+            "title": "STANDART пакет"
+        }
+    },
+    {
+        "name": "Мухин",
+        "surname": "Али",
+        "phone": "+998971350600",
+        "payment": 300,
+        "id": "mFM1k",
+        "pack": {
+            "value": "starter",
+            "title": "STARTER пакет"
+        }
+    },
+    {
+        "name": "Роман",
+        "surname": "Романов",
+        "phone": "+998936512526",
+        "payment": 1000,
+        "id": "savMk",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    },
+    {
+        "name": "Виктория",
+        "surname": "Мартыновская",
+        "phone": "+998946509550",
+        "payment": 1000,
+        "id": "G5leG",
+        "pack": {
+            "value": "vip",
+            "title": "VIP пакет"
+        }
+    }
+]
